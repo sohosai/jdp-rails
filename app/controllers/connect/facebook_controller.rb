920f8688997f71a7f3964ef1b7f8f2199584a139
polamjag <s@polamjag.info>
2015-01-19 16:28:22 +0900

    bundle update

--- app/controllers/connect/facebook_controller.rb
+++
@@ -1,8 +0,0 @@
-class Connect::FacebookController < ApplicationController
-  before_filter :require_anonymous_access
-
-  def show
-    authenticate Connect::Facebook.authenticate(cookies)
-    logged_in!
-  end
-end
--- app/controllers/connect/google_controller.rb
+++
@@ -1,9 +0,0 @@
-class Connect::GoogleController < ApplicationController
-  before_filter :require_anonymous_access
-
-  def show
-    raise AuthenticationRequired.new('Authorization Code Required') unless params[:code]
-    authenticate Connect::Google.authenticate(params[:code])
-    logged_in!
-  end
-end
